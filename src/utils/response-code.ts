9c940fc11c4df98dd5e5bf152ea232b11bba6c6f
mannakub12 <mannakub1@gmail.com>
2022-07-09 02:51:23 +0700

    edit: validate error code

--- src/utils/response-code.ts
+++ src/utils/response-code.ts
@@ -1,29 +1,60 @@
+//base
 const _validateError = 100000
-
 export const InternalSeverError = _validateError + 1
 export const InvalidJSONString = _validateError + 2
-export const UnableRegisterEmailAlreayExist = _validateError + 4
-export const UnableRegisterUsernameAlreayExist = _validateError + 5
-export const UnableInsertMemberToDbError = _validateError + 6
-export const UnableVerifyOtpIncorrect = _validateError + 7
-export const UnableVerifyOtpDataNotfound = _validateError + 8
-export const UnableVerifyOtpLimitExceeded = _validateError + 9
-export const UnableVerifyOtpIsAreadyVerified = _validateError + 10
-export const UnableToSendOtp = _validateError + 11
-export const UnableLoginUsernameDoNotAlreayExist = _validateError + 12
-export const PasswordIsInvalid = _validateError + 13
-export const OldPassowrdInvalid = _validateError + 14
-export const UnableUpatePasswordToDb = _validateError + 15
-export const UnableToAddMobile = _validateError + 16
-export const UnableToSetMainMobile = _validateError + 17
-export const UnableToDeleteMobile = _validateError + 18
-export const UnableToUpdateEmailToDb = _validateError + 19
-export const UnableToSendEmail = _validateError + 20
-export const UnableInsertAddressToDb = _validateError + 21
-export const UnableUpdateAddressToDb = _validateError + 22
-export const UnableDeleteAddressById = _validateError + 23
-export const UnableUpdateNotMainAddressByMemberId = _validateError + 24
-export const UnableUpdateIsMainAddressById = _validateError + 25
-export const UnableInquiryAddressById = _validateError + 26
-export const UnableInquiryAddressesByMemberId = _validateError + 27
-export const UnableUpateProfileToDb = _validateError + 28
+
+// auth module
+
+// auth service
+const _authError = _validateError + 101000
+export const UnableRegisterEmailAlreayExist = _authError + 1
+export const UnableRegisterUsernameAlreayExist = _authError + 2
+export const UnableInsertMemberToDbError = _validateError + 3
+
+// login
+export const UnableLoginUsernameDoNotAlreayExist = _authError + 4
+export const PasswordIsInvalid = _authError + 5
+
+// otp module
+const _otpError = _validateError + 102000
+export const UnableVerifyOtpIncorrect = _otpError + 1
+export const UnableVerifyOtpDataNotfound = _otpError + 2
+export const UnableVerifyOtpLimitExceeded = _otpError + 3
+export const UnableVerifyOtpIsAreadyVerified = _otpError + 4
+export const UnableToSendOtp = _otpError + 5
+
+// member module
+const _memberError = _validateError + 103000
+
+// member service
+export const UnableUpateProfileToDb = _memberError + 1
+
+// email service
+export const OldPassowrdInvalid = _memberError + 2
+export const UnableToUpdateEmailToDb = _memberError + 3
+export const UnableToSendEmail = _memberError + 4
+
+// password service
+export const UnableUpatePasswordToDb = _memberError + 5
+
+// mobile module
+const _mobileError = _validateError + 104000
+export const UnableToAddMobile = _mobileError + 1
+export const UnableToSetMainMobile = _mobileError + 2
+export const UnableToDeleteMobile = _mobileError + 3
+
+// address module
+const _addressError = _validateError + 105000
+export const UnableInsertAddressToDb = _addressError + 1
+export const UnableUpdateAddressToDb = _addressError + 2
+export const UnableDeleteAddressById = _addressError + 3
+export const UnableUpdateNotMainAddressByMemberId = _addressError + 4
+export const UnableUpdateIsMainAddressById = _addressError + 5
+export const UnableInquiryAddressById = _addressError + 6
+export const UnableInquiryAddressesByMemberId = _addressError + 7
+
+// seller module
+const _sellerError = _validateError + 106000
+
+// review module
+const _reviewError = _validateError + 107000
