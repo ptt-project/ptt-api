4600258b4c023260409f24440728343bea56c07c
mannakub12 <mannakub1@gmail.com>
2022-08-15 20:22:51 +0700

    add: AccessToken and RefreshToken for set cookie

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -61,11 +61,11 @@ export class AuthController {
 
     const accessToken = `AccessToken=${
       longinResponse.data.accessToken
-    }; HttpOnly; Max-Age=${dayjs().add(1, 'day')}; SameSite=Strict;`
+    }; Max-Age=${dayjs().add(1, 'day')};`
 
     const refreshToken = `RefreshToken=${
       longinResponse.data.refreshToken
-    }; HttpOnly; Max-Age=${dayjs().add(7, 'day')}; SameSite=Strict;`
+    }; Max-Age=${dayjs().add(7, 'day')};`
 
     request.res.setHeader('Set-Cookie', [accessToken, refreshToken])
     // request.res.setHeader('AccessToken', accessToken)
