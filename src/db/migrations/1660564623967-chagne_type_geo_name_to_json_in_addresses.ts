7ffc888422eebed5714c5288d9af39f53ed06f71
mannakub12 <mannakub1@gmail.com>
2022-08-15 19:10:57 +0700

    delete: drop index

--- src/db/migrations/1660564623967-chagne_type_geo_name_to_json_in_addresses.ts
+++ src/db/migrations/1660564623967-chagne_type_geo_name_to_json_in_addresses.ts
@@ -1,18 +1,20 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm'
 
-export class chagneTypeGeoNameToJsonInAddresses1660564623967 implements MigrationInterface {
-    name = 'chagneTypeGeoNameToJsonInAddresses1660564623967'
+export class chagneTypeGeoNameToJsonInAddresses1660564623967
+  implements MigrationInterface {
+  name = 'chagneTypeGeoNameToJsonInAddresses1660564623967'
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP INDEX "public"."members_username_uindex"`);
-        await queryRunner.query(`ALTER TABLE "addresses" DROP COLUMN "geo_name"`);
-        await queryRunner.query(`ALTER TABLE "addresses" ADD "geo_name" text DEFAULT '{}'`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "addresses" DROP COLUMN "geo_name"`)
+    await queryRunner.query(
+      `ALTER TABLE "addresses" ADD "geo_name" text DEFAULT '{}'`,
+    )
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "addresses" DROP COLUMN "geo_name"`);
-        await queryRunner.query(`ALTER TABLE "addresses" ADD "geo_name" character varying`);
-        await queryRunner.query(`CREATE UNIQUE INDEX "members_username_uindex" ON "members" ("username") `);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "addresses" DROP COLUMN "geo_name"`)
+    await queryRunner.query(
+      `ALTER TABLE "addresses" ADD "geo_name" character varying`,
+    )
+  }
 }
